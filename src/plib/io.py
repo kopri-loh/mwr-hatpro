9467f632b29b4a59d5c77446ddde59e68cf83d91
Loren Oh <loh@kopri.re.kr>
2022-12-16 14:03:03 +0900

    Update file I/O ops

--- src/plib/io.py
+++ src/plib/io.py
@@ -1,4 +1,5 @@
 import struct as st
+import json
 
 
 # Define getter
@@ -11,3 +12,18 @@ def unpack(bd, pos, type="i"):
         return st.unpack(type, bd[pos : pos + 1])
     else:
         raise TypeError("Invalid Type Reference")
+
+
+def get_ret():
+    with open("../ret.json") as f_json:
+        ret = json.load(f_json)
+
+    return ret
+
+
+def to_csv(df, f_path, desc):
+    # Add comment and write CSV
+    with open(f_path, "w") as f:
+        f.write(f"{desc} \n")
+
+    df.to_csv(f_path, mode="a", index=False)
